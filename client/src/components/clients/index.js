6b9617a4f3d28569b526631f28fd7e57875a94a3
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2018-06-24 18:36:47 +0100

    Navbar for client homepage completed

--- client/src/components/clients/index.js
+++ client/src/components/clients/index.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import  AppBar  from '@material-ui/core/AppBar';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class ClientIndex extends Component {
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    flex: {
+      flex: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
+
+  
+class ClientIndex extends Component {
 
     render() {
+
+        const { classes } = this.props;
 
         return (
 
             <div className="ClientIndex">
-                ClientIndex rendered
+                <AppBar>
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" color="inherit" className={classes.flex}>
+                        My Portfolio
+                        </Typography>
+                        
+                            <ul style={{ display: "flex", flex: "1", listStyle: "none" }}>
+                                <li variant="title" color="inherit" className={classes.flex}>
+                                    HOME
+                                </li>
+                                <li variant="title" color="inherit" className={classes.flex}>
+                                    ABOUT
+                                </li>
+                                <li variant="title" color="inherit" className={classes.flex}>
+                                    SERVICES
+                                </li>
+                                <li variant="title" color="inherit" className={classes.flex}>
+                                    WORKS
+                                </li>
+                                <li variant="title" color="inherit" className={classes.flex}>
+                                    CONTACT ME
+                                </li>
+                            </ul>
+                        
+                    </Toolbar>
+                </AppBar>
             </div>
 
         );
@@ -15,3 +67,5 @@ export default class ClientIndex extends Component {
     }
 
 }
+
+export default withStyles(styles)(ClientIndex);
