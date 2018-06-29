873560ac470790e9cce1df48976cfa42bc29feb4
Qasim Abdulkadir <kazeem.kadiri@gmail.com>
2018-06-29 17:08:46 +0100

    Recent form update

--- client/src/components/clients/contact-form.js
+++ client/src/components/clients/contact-form.js
@@ -1,9 +1,12 @@
 import React,{ Component } from'react';
 import { withStyles } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import TextField from '@material-ui/core/TextField';
 
 
-const styles = () => (
+const styles = (theme) => (
 
     {
         container: {
@@ -25,8 +28,21 @@ const styles = () => (
         },
         fullWidth: {
             width: "100%"
+        },
+        sendButton: {
+            background: "#0c2e8a"
+        },
+        button: {
+            margin: theme.spacing.unit,
+        },
+        textField: {
+            marginLeft: theme.spacing.unit,
+            marginRight: theme.spacing.unit,
+            width: 200,
+        },
+        formContainer:{
+            justifyContent: "center"
         }
-
     }
 
 )
@@ -53,27 +69,6 @@ class ContactForm extends Component {
 
         let newFormFieldValue = {};
 
-/*        switch(formField) {
-
-            case "name":
-                newFormFieldValue = { "name": value };
-                break;
-
-            case "email":
-                    newFormFieldValue = { "email": value };
-                break;
-
-            case "subject":
-                    newFormFieldValue = { "subject": value };
-                break;
-
-            case "message":
-                    newFormFieldValue = { "message": value };
-                break;
-
-
-        }*/
-
         newFormFieldValue = { [formField]: value }
 
         const { formObject } = this.state;
@@ -88,50 +83,83 @@ class ContactForm extends Component {
 
         const { classes } = this.props;
 
+        const { subject, name, email, message } = this.state.formObject;
+
         return (
 
-               <div className="contactForm">
+               <div className="contactForm" style={{ width: "100%" }}>
 
-                <Grid container spacing={8}>
+                <Grid container spacing={0} className={ classes.formContainer } >
 
                        {/* Grid holds the first two input fields */}
-                       <Grid item xs={12} sm={12} md={12}>
+                       <Grid item xs={12} sm={12} md={8}>
 
-                            <Grid item xs={12} sm={12} md={6}>
-                                <input type="text"
-                                       className={ classes.fullWidth }
-                                       placeholder="Name"
-                                       onKeyUp={ event => this.updateFormParametersObject(event.target.value, "name") } />
-                            </Grid>
+                            <Grid container spacing={0} style={{ justifyContent: "space-between" }}>
+                                <Grid item xs={12} sm={12} md={5}>
+                                    <TextField
+                                          id="name"
+                                          label="Name"
+                                          className={ classes.textField }
+                                          style={{ width: "100%" }}
+                                          value={ name }
+                                          onChange={ event => this.updateFormParametersObject(event.target.value, "name") }
+                                          margin="normal"
+                                        />
+                                </Grid>
 
 
-                            <Grid item xs={12} sm={12} md={6}>
-                                <input type="text"
-                                   className={ classes.fullWidth }
-                                   placeholder="Email"
-                                   onKeyUp={ event => this.updateFormParametersObject(event.target.value, "email") } />
+                                <Grid item xs={12} sm={12} md={5}>
+                                    <TextField
+                                          id="email"
+                                          label="Email"
+                                          className={ classes.textField }
+                                          style={{ width: "100%" }}
+                                          value={ email }
+                                          onChange={ event => this.updateFormParametersObject(event.target.value, "email") }
+                                          margin="normal"
+                                        />
+
+                                </Grid>
                             </Grid>
 
                        </Grid>
 
 
                        {/* Grid holds the first two input fields */}
-                       <Grid item xs={12} sm={12} md={12}>
-
-                          <input className={ classes.fullWidth }
-                                 type="text"
-                                 placeholder="subject"
-                                 onKeyUp={ event => this.updateFormParametersObject(event.target.value, "subject") } />
+                       <Grid item xs={12} sm={12} md={8}>
+                          <TextField
+                                id="subject"
+                                label="Subject"
+                                className={ classes.textField }
+                                style={{ width: "100%" }}
+                                value={ subject }
+                                onChange={ event => this.updateFormParametersObject(event.target.value, "subject") }
+                                margin="normal"
+                              />
 
                        </Grid>
 
                        {/* Grid holds the first two input fields */}
+                       <Grid item xs={12} sm={12} md={8}>
+                               <TextField
+                                         id="multiline-static"
+                                         label="Message"
+                                         multiline
+                                         rows="4"
+                                         defaultValue={ message }
+                                         className={classes.textField}
+                                         style={{ width: "100%" }}
+                                         onChange={ event => this.updateFormParametersObject(event.target.value, "message") }
+                                         margin="normal"
+                                       />
+
+                       </Grid>
+
                        <Grid item xs={12} sm={12} md={12}>
 
-                               <textarea row="4"
-                                         className={ classes.fullWidth }
-                                         onKeyUp={ event => this.updateFormParametersObject(event.target.value, "message") }
-                                         ></textarea>
+                           <Button variant="contained" color="primary" className={`sendButton ${classes.button}`}>
+                                <Icon>send</Icon>&nbsp; &nbsp; Send
+                           </Button>
 
                        </Grid>
                 </Grid>
