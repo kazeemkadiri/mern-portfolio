0f7bd1477d72b8f727a86076e25b8e8562b36a17
Qasim Abdulkadir <kazeem.kadiri@gmail.com>
2018-07-02 22:24:15 +0100

    View project updates

--- client/src/components/admin/view-project.js
+++ client/src/components/admin/view-project.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import { keys } from '../../keys';
 
 import  withStyles  from '@material-ui/core/styles/withStyles'
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-
+import { Carousel } from 'react-responsive-carousel';
 
 
 const styles = theme => ({
@@ -32,28 +33,99 @@ class AddProject extends Component{
         super()
 
         this.state = {
-            formObject:{
-                title: '',
-                description: '',
-                implementation_details: ''
+            
+            project: {
+                title: "Custom cms project",
+                description: "A website management system",
+                slides:[
+                ]
+            },
+            slide:{
+                title:"",
+                description: "",
+                image_path: ""
+                
             }
 
         }
 
     }
 
-    updateFormParametersObject(value, formField) {
+    addSlide() {
 
-        const { formObject } = this.state;
+        const { project, project: { slides } } = this.state;
+
+        slides.push(this.state.slide);
 
         this.setState({
-            "formObject": {
-                ...formObject,
+
+            "project": {
+                ...project,
+                "slides":slides
+            }
+
+        })
+
+        this.setState({
+            slide:{
+                title:"",
+                description: "",
+                image_path: ""
+            }
+        })
+        
+
+        console.log(this.state.project);
+
+    }
+
+    updateFormParametersObject(value, formField) {
+
+        const { slide } = this.state;
+
+        this.setState({
+            "slide": {
+                ...slide,
                 [formField]: value
             }
         }) 
 
-        console.log(this.state.formObject);
+        console.log(this.state.slide);
+
+    }
+
+    deleteSlide(slide) {
+
+        console.log(slide);
+
+        // Make a graphql mutation call to delete slide
+
+        //Remove slide from the list of slides in project
+        const { project, project: { slides } } = this.state;
+
+        const slideIndex = slides.indexOf(slide);
+
+        (slideIndex > -1) ? slides.splice(slideIndex, 1) : '';
+
+        // Update the state of slides
+        this.setState({
+            "project":{
+                ...project,
+                "slides": slides
+            }
+        })
+        
+    }
+
+    editSlide(slide) {
+
+        console.log("editing slide", slide);
+
+        // Confirm if currently working on attaching a new slide
+
+        this.setState({
+            slide: slide
+        })
 
     }
 
@@ -63,53 +135,91 @@ class AddProject extends Component{
 
         const { classes } = this.props;
 
-        const { formObject: { title, description, implementation_details } } = this.state;
+        const { project, slide: { image_path, title } } = this.state;
 
         return (
             <Grid container spacing={0} className={ classes.topPageStyles }>
-                            <Grid item xs={12} sm={12} md={8}>
-                                <Grid container spacing={0} className={ classes.topPageStyles }>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                        <TextField
-                                            id="title"
-                                            label="Title"
-                                            className={ classes.textField }
-                                            style={{ width: "100%" }}
-                                            value={ title }
-                                            onChange={ event => this.updateFormParametersObject(event.target.value, "title") }
-                                            margin="normal"
-                                            />
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={12}>
-                                        <TextField
-                                            id="description"
-                                            label="Description"
-                                            multiline
-                                            rows="3"
-                                            className={ classes.textField }
-                                            style={{ width: "100%" }}
-                                            value={ description }
-                                            onChange={ event => this.updateFormParametersObject(event.target.value, "description") }
-                                            margin="normal"
-                                            />
-                                    </Grid>
+                
+                <Grid item xs={12} sm={12} md={8}>
+                    <h4> Title: </h4> { project.title }
+                    <h4> Description: </h4> { project.description }
+                </Grid>
 
-                                    <Grid item xs={12} sm={12} md={12}>
-                                        {/* Editor to save new project */}
-                                        <Editor
-                                                apiKey="sa36zz9cj49l40y40bjt5xoy6xtu1hiansfynn2p79spjez1"
-                                                initialValue="<p>This is the initial content of the editor</p>"
-                                                init={{
-                                                plugins: 'link image code',
-                                                toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-                                                }}
-                                                onChange={event => this.updateFormParametersObject(event.target.getContent(),
-                                                                     "implementation_details") }
-                                            />
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </Grid>
+                <Grid item xs={12} sm={12} md={8}>
+                    {/* Carousel slides of the project images are rendered here */}
+                    <Carousel>
+                        { (project.slides.length > 0) ? project.slides.map( slide => ( 
+                            
+                            <div style={{ position: "relative", height: "100%" }} key={slide}>
+                                
+                                <img src = { slide.image_path } 
+                                    alt = {slide.title} 
+                                    style = {{ height: "100%" }} />
+
+                                <p className="legend">
+                                    { slide.title }
+                                    
+                                    {/* Slide delete button */}
+                                    <Button variant="contained" 
+                                            color="primary" 
+                                            className={classes.button}
+                                            onClick = { () => this.deleteSlide(slide) }>
+                                        <Icon></Icon>Delete
+                                    </Button>
+
+                                    {/* Slide edit button */}
+                                    <Button variant="contained" 
+                                            color="primary" 
+                                            className={classes.button}
+                                            onClick = { () => this.editSlide(slide) }>
+                                        <Icon></Icon>Edit
+                                    </Button>
+                                 </p>
+
+                            </div>
+                            
+                            )
+                            ): ""
+                        }
+                    </Carousel>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={8}>
+                    <Button variant="contained" 
+                            color="primary" 
+                            className={classes.button}
+                            onClick = { () => this.addSlide() }>
+                        <Icon></Icon>Attach Slide
+                    </Button>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={8}>
+                    <TextField
+                        id="slidetitle"
+                        label="Slide title"
+                        className={ classes.textField }
+                        style={{ width: "100%" }}
+                        value={ title }
+                        onChange={ event => this.updateFormParametersObject(event.target.value, "title") }
+                        margin="normal"
+                        />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={8}>
+                    {/* Editor to attach new slide for project */}
+                    <Editor
+                            apiKey={ keys.tinymce_api_key }
+                            initialValue={ image_path }
+                            init={{
+                            plugins: 'link image code',
+                            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                            }}
+                            onChange={event => this.updateFormParametersObject(event.target.getContent(),
+                                                    "slide_image") }
+                        />
+                </Grid>
+
+            </Grid>
         )
 
     }
