c19e9b95b625eccd7c7f504d139cd96843db5de9
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2018-07-14 11:48:22 +0100

    Created seeds for mongodb

---
+++ server/seeds/default.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt')
+const BioModel = require('../models/bio')
+const UserModel = require('../models/user')
+const ProjectsModel = require('../models/projects')
+const ServiceModel = require('../models/services')
+
+//User seed 
+const hashedPassword = await bcrypt.hash('rats', 10);
+
+await UserModel.create({
+    'email': 'kazeem.kadiri@gmail.com',
+    'password': hashedPassword
+})
+
+//Bio seed
+await BioModel.create([{
+    'description': 'Random description',
+    'header_bg_img': '<img src="blah" />',
+    'about_me_img': '<img src="blah" />',
+    'header_bg_img_text': 'Some random text',
+    'phone_no': '9394024234',
+    'email': 'kazeem.kadiri@gmail.com'
+}])
+
+await ProjectModel.create([{
+    'title': 'Project 1',
+    'description': 'Project description',
+    'implementation_details': 'Built using Mern stack',
+    'slides': []
+}])
+
+await ServiceModel.create([{
+    'title': 'Service 1',
+    'description': 'Service description',
+    'service_img': '<img src= "sdff" />'
+}])
+ 
+
+
